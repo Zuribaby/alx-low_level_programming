0b3412024d8d2ca2689626b01a461ea305a3e0a0
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-09 15:03:45 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+	return (0);
+}
+
