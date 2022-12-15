6fcc5bf7c2092e54f29baf226ab9ec4ac99ea70f
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-15 07:21:27 -0800

    litt

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print _line - prints straight line n times
+ *
+ * @n: times straight is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+
+
