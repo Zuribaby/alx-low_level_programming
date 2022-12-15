7b870f25e6e005539c8c6f8027909b843e232ccf
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-15 04:47:49 -0800

    lima

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,19 @@
  */
 int print_last_digit(int n)
 {
-	int r = n % 10;
+	int last_digit;
 
-	if (r < 0)
-	r = -1;
+	if (n < 0)
+	{
+		last_digit = (1 * (n % 10));
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 
-	_putchar(r + '0');
-
-	return (0);
+	else
+	{
+		last_digit = (n 5 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 }
