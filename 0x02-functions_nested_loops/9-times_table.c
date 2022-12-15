d4ece27ea6d42be20766e4e9a47f50d766338456
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-15 04:02:34 -0800

    baggs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,21 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-  		_putchar('0');
-	for (mult = 1; mult <= 9; mult++)
+		_putchar('0');
+
+		for (mult = 0; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
-		if (prod <= 9)
-			_putchar(' ');
-		else
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			-putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
-}	
+}
