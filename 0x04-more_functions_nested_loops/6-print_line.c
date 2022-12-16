5bf09d76b3c1d146b2c9a4bcb04b9a28bb0e0131
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-15 19:38:24 -0800

    jugs

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,12 @@
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
 }
-
+_putchar('\n');
+}
 
 
