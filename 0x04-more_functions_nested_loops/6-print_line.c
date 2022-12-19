60f3b4b72ffba5203ad3254f1dff9a65718ac65e
Zuribaby <chiamakanwobodo07@gmail.com>
2022-12-19 01:39:02 -0800

    spoon

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,15 +4,22 @@
  * print _line - the shortest distance between two points is a straight line
  * @n: times straight is printed
  *
- * Return: no return
+ * Return: nothing on success
  */
 void print_line(int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
+	int count = 0;
 
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (count < n)
+		{
+			_putchar('_');
+			count++;
+		}
+		_putchar('\n');
+	}
 }
+
